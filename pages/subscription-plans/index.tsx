bad66ae5ad4f87cb11d34c46adb00458b1734e2f
yunis-ali <yunisali1@gmail.com>
2020-12-05 21:57:57 +0000

    simplified index page.

--- pages/subscription-plans/index.tsx
+++ pages/subscription-plans/index.tsx
@@ -1,26 +1,3 @@
-import Grid from '@material-ui/core/Grid';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-
 import { DefaultState } from './index.stories'
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-  }),
-);
-
-export default function Home() { 
-    const classes = useStyles();
-
-    return (
-        <>
-            <div className={classes.root}>
-                <Grid>
-                    <DefaultState />
-                </Grid>
-            </div>
-        </>
-    )
-}
+export default function Home() { return (<DefaultState />) }
